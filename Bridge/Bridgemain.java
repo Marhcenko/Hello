b2500f2e3323263b34109f11ba0a564377b637cc
Marchenko <qamarhcneko@gmail.com>
2017-03-15 13:06:32 +0200

    Bridge

---
+++ Bridge/Bridgemain.java
@@ -0,0 +1,78 @@
+package Bridge;
+
+/**
+ * Created by SHYRIK on 16.02.2017.
+ */
+public class Bridgemain {
+    public static void main(String[] args) {
+//       Car c = new Sedan(new Mersedes());
+//       c.show();
+
+
+    }
+}
+//interface Make {
+//    void SetMake();
+//}
+//
+//class Kia implements Make{
+//    @Override
+//    public void SetMake() {
+//        System.out.println(" машина  - киа");
+//    }
+//}
+//class Hyundai implements Make {
+//
+//    @Override
+//    public void SetMake() {
+//        System.out.println("машина  - хундай");
+//    }
+//}
+//class Mersedes implements Make {
+//
+//    @Override
+//    public void SetMake() {
+//        System.out.println("машина  -  мерседес");
+//    }
+//}
+//
+//abstract  class Car {
+//    Make make;
+//    public Car(Make make) {
+//        this.make = make;
+//    }
+//   abstract void show();
+//}
+//
+//class Hutchback extends Car {
+//
+//    public Hutchback(Make make) {
+//        super(make);
+//    }
+//
+//    @Override
+//    void show() {
+//        make.SetMake();
+//        System.out.println("тип машини есть - [ Hutchback ]");
+//    }
+//}
+//
+//class Sedan extends Car{
+//    public Sedan(Make make) {
+//        super(make);
+//    }
+//
+//    @Override
+//    void show() {
+//      make.SetMake();
+//      System.out.println("тип машинни - [ Sedan ]");
+//    }
+//}
+
+
+
+
+
+
+
+
