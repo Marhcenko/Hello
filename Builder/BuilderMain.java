7236d7b14a222c1d8f991ca73f62e57e2db4c903
Marchenko <qamarhcneko@gmail.com>
2017-03-15 13:22:15 +0200

    builder

---
+++ Builder/BuilderMain.java
@@ -0,0 +1,77 @@
+package Builder;
+
+/**
+ * Created by SHYRIK on 17.02.2017.
+ */
+public class BuilderMain {
+    public static void main(String[] args) {
+//        Car car = new BuilderCar()
+//                      .BuildName("Mersedes")
+//                      .BuildSpeed(200)
+//                      .Buildtransmisia(Transmisia.Auto)
+//                      .Build();
+//
+//        System.out.println(car);
+
+
+    }
+}
+
+//enum Transmisia {
+//    Auto,Manualo
+//}
+//
+//class Car {
+//    String name;
+//    Transmisia transmisia;
+//    int maxSpeed;
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public void setTransmisia(Transmisia transmisia) {
+//        this.transmisia = transmisia;
+//    }
+//
+//    public void setMaxSpeed(int maxSpeed) {
+//        this.maxSpeed = maxSpeed;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Car{" +
+//                "name='" + name + '\'' +
+//                ", transmisia=" + transmisia +
+//                ", maxSpeed=" + maxSpeed +
+//                '}';
+//    }
+//}
+//
+//class BuilderCar {
+//    String n;
+//    Transmisia t;
+//    int s;
+//
+//    BuilderCar BuildName(String n){
+//        this.n = n;
+//        return this;
+//    }
+//
+//    BuilderCar Buildtransmisia(Transmisia t){
+//        this.t = t;
+//        return this;
+//    }
+//    BuilderCar BuildSpeed(int s){
+//        this.s = s;
+//        return this;
+//    }
+//
+//    Car Build(){
+//     Car car = new Car();
+//     car.setName(n);
+//     car.setTransmisia(t);
+//     car.setMaxSpeed(s);
+//     return  car;
+//    }
+//}
